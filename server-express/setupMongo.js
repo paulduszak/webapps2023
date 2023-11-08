1b9a4e9e046edd8ebb4703ebcfbc2b3c3867f715
Paul Duszak <duszak.paul@gmail.com>
2023-11-08 02:50:46 +0000

    End week 9 lecture

---
+++ server-express/setupMongo.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const uri = "";
+function connect() {
+  const options = { useNewUrlParser: true };
+  mongoose.connect(uri, options).then(
+    () => {
+      console.log("Database connection established!");
+    },
+    (err) => {
+      console.log("Error connecting Database instance due to: ", err);
+    }
+  );
+}
+module.exports = connect;
